e0dace2f1520a7bb75d95f26aa3800863f3f220b
Kyle Everett <keverett@student.wethinkcode.co.za>
2019-09-10 11:56:27 +0200

    ft_elstdel.c added

--- minishell.h
+++ minishell.h
@@ -5,6 +5,7 @@
 #	include "libft/libft.h"
 #	include <unistd.h>
 #	include <stdlib.h>
+#	include <limits.h>
 # define BUFF_SIZE 2048
 
 typedef struct s_e_list
@@ -19,5 +20,6 @@ void		ft_t_add(t_e_list *lst, t_e_list *new);
 t_e_list	*ft_t_new(char *str, int len);
 int 		ft_env(t_e_list *lst);
 t_e_list 	*ft_t_vnew(char *name, char *value);
+void		ft_elstdel(t_e_list *lst);
 
 #endif
