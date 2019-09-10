79f14cbffc7e6d69a7241673e724c2030732f731
Kyle Everett <keverett@student.wethinkcode.co.za>
2019-09-10 08:31:34 +0200

    Added freeing to Ft_env

--- ft_env.c
+++ ft_env.c
@@ -2,13 +2,20 @@
 
 int    ft_env(t_e_list *lst)
 {
+	t_e_list *tmp;
+
+	
     while (lst)
     {
-        ft_putstr(lst->name);
-        ft_putchar('=');
+    	tmp = lst;
+	    ft_putstr(lst->name);
+        free(lst->name);
+		ft_putchar('=');
         ft_putstr(lst->value);
-        ft_putchar('\n');
+        free(lst->value);
+		ft_putchar('\n');
         lst = lst->next;
+		free(tmp);
 	}
 	return 0;
 }
