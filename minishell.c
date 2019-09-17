15a9760dcd4330dc6d8c6738f783976079001e97
zhelm <zhelm@student.wethinkcode.co.za>
2019-09-17 17:32:47 +0200

    Some stuff Kyle wanted. Cant test here at home though. I will test and fix bugs tomorrow

--- minishell.c
+++ minishell.c
@@ -16,7 +16,7 @@ void ft_arg_split(t_args_2d **input, char *tmp)
 		{
 			while (tmp[i + j] && ft_iswhitespace(tmp[i + j]) != 1)
 				j++;
-			if((*input)->node == NULL)
+			if ((*input)->node == NULL)
 				(*input)->node = ft_t_args_new(ft_strsub(tmp, i, j));
 			else
 				ft_t_args_add(&((*input)->node), ft_t_args_new(ft_strsub(tmp, i, j)));
@@ -27,7 +27,7 @@ void ft_arg_split(t_args_2d **input, char *tmp)
 			i++;
 			while (tmp[i + j] && tmp[i + j] != '"')
 				j++;
-			if((*input)->node == NULL)
+			if ((*input)->node == NULL)
 				(*input)->node = ft_t_args_new(ft_strsub(tmp, i, j));
 			else
 				ft_t_args_add(&((*input)->node), ft_t_args_new(ft_strsub(tmp, i, j)));
@@ -65,17 +65,17 @@ int ft_read_args(t_args_2d **input_2d, int count)
 	i = 0;
 	tmp = readline("$> ");
 	add_history(tmp);
-	if(tmp)
+	if (tmp)
 	{
-	while (tmp[i])
-	{
-		if (tmp[i] == '"')
-			count = (count == 0) ? 1 : 0;
-		i++;
-	}
-	if (count == 1)
-		tmp = ft_read_dquote(tmp, count);
-	ft_arg_split(input_2d, tmp);
+		while (tmp[i])
+		{
+			if (tmp[i] == '"')
+				count = (count == 0) ? 1 : 0;
+			i++;
+		}
+		if (count == 1)
+			tmp = ft_read_dquote(tmp, count);
+		ft_arg_split(input_2d, tmp);
 	}
 	return 1;
 }
@@ -87,7 +87,8 @@ int main() //echo, cd, setenv, unsetenv, env, exit
 	extern char **environ;
 	t_args_2d *input_2d;
 	t_env_list *env;
-	pid_t pid;
+	// pid_t pid;
+	struct stat sb;
 
 	input_2d = NULL;
 	env = ft_splitenv(environ);
@@ -97,33 +98,37 @@ int main() //echo, cd, setenv, unsetenv, env, exit
 		if (input_2d->node)
 		{
 			arg = input_2d->node->argument;
-			if(arg)//going to activate each time
+			if (arg) //going to activate each time
 			{
-				if (ft_strcmp(arg , "echo") != 0 && ft_strcmp(arg, "exit") != 0 && ft_strcmp(arg, "setenv") != 0 && ft_strcmp(arg, "cd") != 0 && ft_strcmp(arg, "unsetenv") != 0 && ft_strcmp(arg, "env") != 0)
-					{
-						if(ft_findpath(input_2d->node->argument, env) != NULL)
-						{
-							pid = fork();
-							if(pid == 0)
-								execve(ft_findpath(input_2d->node->argument, env), ft_t_lst_array(input_2d->node), ft_lstarray(env));
-							else
-								wait(NULL);
-						}
-					}
+				// if (ft_strcmp(arg, "echo") != 0 && ft_strcmp(arg, "exit") != 0 && ft_strcmp(arg, "setenv") != 0 && ft_strcmp(arg, "cd") != 0 && ft_strcmp(arg, "unsetenv") != 0 && ft_strcmp(arg, "env") != 0)
+				// {
+				// 	if (lstat(arg, &sb) != -1)
+				// 	{
+				// 		if (ft_findpath(input_2d->node->argument, env) != NULL)
+				// 		{
+				// 			pid = fork();
+				// 			if (pid == 0)
+				// 				execve(ft_findpath(input_2d->node->argument, env), ft_t_lst_array(input_2d->node), ft_lstarray(env));
+				// 			else
+				// 				wait(NULL);
+				// 		}
+				// 	}
+				// }
+				if (input_2d->node->argument &&  ft_strcmp(input_2d->node->argument, "exit") == 0) //no other arguments otherwise "error exit: too many arguments" message;
+				{
+					ft_elstdel(env);
+					return (0);
+				}
+				else if (ft_strcmp("env", input_2d->node->argument) == 0)
+					ft_env(env);
+				else if (ft_strcmp(input_2d->node->argument, "echo") == 0)
+					ft_echo(input_2d->node);
+				else if(ft_strcmp("setenv", arg) == 0)
+					ft_setenv(&env, input_2d->node->next->argument, input_2d->node->next->next->argument, 1);//all chars
+				else if (ft_strcmp("unsetenv", arg) == 0)
+					ft_unsetenv(env, input_2d->node->next->argument);
+				input_2d->node = input_2d->node->next;
 			}
-			// else if (input_2d->node->argument &&  ft_strcmp(input_2d->node->argument, "exit") == 0) //no other arguments otherwise "error exit: too many arguments" message;
-			// {
-			// 	ft_elstdel(env);
-			// 	return (0);
-			// }
-			// if (ft_strcmp("env", input_2d->node->argument) == 0)
-			// 	ft_env(env);
-			// if (ft_strcmp(input_2d->node->argument, "echo") == 0)
-			// 	ft_echo(input_2d->node);
-			// // // if(ft_strstr())
-			// // 	ft_setenv(&env, envar, enval);//all chars
-			// input_2d->node = input_2d->node->next;
-		}
 			input_2d = input_2d->next;
+		}
 	}
-}
