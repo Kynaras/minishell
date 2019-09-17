8ee93958f14b787b158d1c814f9c6ceb1a61d214
Kyle Everett <keverett@student.wethinkcode.co.za>
2019-09-17 10:14:46 +0200

    Time, permcheck and findpath edited

--- norm.c
+++ norm.c
@@ -1,27 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_time.c                                          :+:      :+:    :+:   */
+/*   norm.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: keverett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/16 16:50:03 by keverett          #+#    #+#             */
-/*   Updated: 2019/09/16 16:50:06 by keverett         ###   ########.fr       */
+/*   Created: 2019/09/16 08:57:04 by keverett          #+#    #+#             */
+/*   Updated: 2019/09/16 08:57:06 by keverett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-#include <time.h>
 
-void	ft_time(void)
-{
-	time_t	t;
-	char	**clock;
-
-	time(&t);
-	clock = ft_strsplit(ctime(&t), ' ');
-	write(1, clock[3], 5);
-	ft_putchar(' ');
-	ft_freearray(clock);
-	return ;
-}
