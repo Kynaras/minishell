3d80c34131f29178b1ff1a326efe923e00feb758
Kyle Everett <keverett@student.wethinkcode.co.za>
2019-09-19 11:17:01 +0200

    Deleted unused files. Norm + ft_cd upgraded.

--- ft_cd.c
+++ ft_cd.c
@@ -17,13 +17,21 @@ void	ft_cd(char *destination, t_env_list *env)
 	char buf[PATH_MAX + 1];
 	char *temp;
 
+	temp = NULL;
 	if (!destination)
 	{
 		ft_setenv(env, "OLDPWD", ft_getenv("PWD", env), 1);
 		ft_setenv(env, "PWD", ft_getenv("HOME", env), 1);
 		return ;
 	}
-	if (!ft_strcmp(destination, "-"))
+	if (destination[0] == '~' && destination[1] == '/' && destination[2] != '\0')
+	{
+		ft_join(&temp, ft_getenv("HOME", env));
+		ft_join(&temp, "/");
+		ft_join(&temp, destination + 2);
+		destination = temp;	
+	}
+	else if (!ft_strcmp(destination, "-"))
 	{
 		if (!ft_strcmp(ft_getenv("OLDPWD", env), ft_getenv("PWD", env)))
 		{
@@ -35,19 +43,21 @@ void	ft_cd(char *destination, t_env_list *env)
 			temp = ft_strdup(ft_getenv("OLDPWD", env));
 			ft_setenv(env, "OLDPWD", ft_getenv("PWD", env), 1);
 			ft_setenv(env, "PWD", temp, 1);
+			chdir(temp);
+			ft_putendl(temp);
 			free(temp);
 			return ;
 		}
 	}
-	chdir(ft_getenv("PWD", env));
 	if (chdir(destination) < 0)
-	{
 		ft_finderror(destination);
-	}
 	else
 	{
+		ft_putstr("HERE");
 		ft_setenv(env, "OLDPWD", ft_getenv("PWD", env), 1);
 		ft_setenv(env, "PWD", getcwd(buf, PATH_MAX), 1);
 	}
+	if (temp)
+		free(temp);
 	return ;
 }
