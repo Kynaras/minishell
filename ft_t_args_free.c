b2ad582957554709b828a78cd63ed40208c2ac8e
Kyle Everett <keverett@student.wethinkcode.co.za>
2019-09-20 10:32:51 +0200

    Try it

---
+++ ft_t_args_free.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_t_args_free.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keverett <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/20 10:26:27 by keverett          #+#    #+#             */
+/*   Updated: 2019/09/20 10:26:29 by keverett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_t_args_free(t_args **head)
+{
+	t_args *tmp;
+
+	while (*head)
+	{
+		tmp = *head;
+		ft_strdel((*head)->argument);
+		*head = (*head)->next;
+		free(tmp);
+	}
+}
